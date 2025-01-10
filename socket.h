532974fa7fa7910e17d49fd211aa243f1b6b7d6c
Peter Foelsche <uhcu_63@outlook.com>
2025-01-10 07:23:36 -0600

    Add project files.

---
+++ socket.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <system_error>
+#include <cerrno>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+namespace foelsche
+{
+namespace SOCKET
+{
+struct socket
+{	const int m_i;
+	socket(const int domain, const int type, const int protocol)
+		:m_i(::socket(domain, type, protocol))
+	{	if (m_i == -1)
+			throw std::system_error(std::error_code(errno, std::generic_category()), "socket() failed!");
+	}
+	~socket(void)
+	{	close(m_i);
+	}
+	static void bind(const int sockfd, const struct sockaddr *const addr, const socklen_t addrlen)
+	{	if (::bind(sockfd, addr, addrlen) == -1)
+			throw std::system_error(std::error_code(errno, std::generic_category()), "bind() failed!");
+	}
+//int bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
+// int listen(int sockfd, int backlog);
+	static void listen(const int sockfd, const int backlog)
+	{	if (::listen(sockfd, backlog) == -1)
+			throw std::system_error(std::error_code(errno, std::generic_category()), "listen() failed!");
+	}
+};
+}
+}
